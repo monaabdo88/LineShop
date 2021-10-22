10e0c91669a07c562ae74d1d64437f0f4215b9ae
monaabdo88 <monaabdo88@gmail.com>
2021-10-22 16:42:50 +0200

    build project structure

---
+++ dashboard/includes/templates/footer.php
@@ -0,0 +1,27 @@
+<footer class="main-footer">
+    <strong>Copyright &copy; 2014-2021 <a href="https://adminlte.io">AdminLTE.io</a>.</strong>
+    All rights reserved.
+    <div class="float-right d-none d-sm-inline-block">
+      <b>Version</b> 3.1.0
+    </div>
+  </footer>
+  </div>
+<!-- ./wrapper -->
+
+<!-- REQUIRED SCRIPTS -->
+
+<!-- jQuery -->
+<script src="<?= $plugins_cp?>jquery/jquery.min.js"></script>
+<!-- Bootstrap -->
+<script src="<?= $plugins_cp?>bootstrap/js/bootstrap.bundle.min.js"></script>
+<!-- AdminLTE -->
+<script src="<?= $js_cp?>adminlte.js"></script>
+
+<!-- OPTIONAL SCRIPTS -->
+<script src="<?= $plugins_cp?>chart.js/Chart.min.js"></script>
+<!-- AdminLTE for demo purposes -->
+<script src="<?= $js_cp?>demo.js"></script>
+<!-- AdminLTE dashboard demo (This is only for demo purposes) -->
+<script src="<?= $js_cp?>pages/dashboard3.js"></script>
+</body>
+</html>
---
+++ init.php
@@ -0,0 +1,13 @@
+<?php
+//Front End Folders Path
+$tpl            = "includes/templates/";
+$css_front      = "assets/css/";
+$js_front       = "assets/js/";
+$plugins_front  = "assets/plugins/";
+$img_front      = "assets/images/"; 
+//Dashboard Folders Path
+$tpl_cp         = "includes/templates/";
+$css_cp         = "assets/dist/css/";
+$js_cp          = "assets/dist/js";
+$plugins_cp     = "assets/plugins/";
+$img_cp         = "assets/dist/img/";
